6289e6744b49f5187b7394c44623e13325ec2e83
WBitencourt <51727640+WBitencourt@users.noreply.github.com>
2020-12-05 16:02:49 -0300

    Reflesh App 05/12/2020 16:03 (DD/MM/YYYY HH:MM)

---
+++ exercicios-express/api/produto.js
@@ -0,0 +1,14 @@
+module.exports = (app, texto) => {
+  function salvar(req, res) {
+    res.send('Produto > Salvar >' + texto)
+  }
+  
+  function obter(req, res) {
+    res.send('Produto > Obter >' + texto)
+  }
+
+  app.post('/produto', salvar)
+  app.get('/produto', obter)
+
+  return  {salvar, obter}
+}
---
+++ exercicios-express/api/usuario.js
@@ -0,0 +1,9 @@
+function salvar(req, res) {
+  res.send('Usuario > Salvar')
+}
+
+function obter(req, res) {
+  res.send('Usuario > Obter')
+}
+
+module.exports = {salvar, obter}
