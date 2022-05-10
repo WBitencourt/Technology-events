a01aba5c8b610ef2045e80704c2a6eed4eda2823
WBitencourt <51727640+WBitencourt@users.noreply.github.com>
2022-05-10 17:31:57 -0300

    reflesh

--- NLW/server/dist/server.js
+++ NLW/server/dist/server.js
@@ -10,6 +10,6 @@ const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(routes_1.routes);
-app.listen(3333, () => {
+app.listen(process.env.PORT || 3333, () => {
     console.log('HTTP server running!');
 });
