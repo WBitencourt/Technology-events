5dcd09d451f3d38d4d6f0311bcb240554bac08bf
WBitencourt <51727640+WBitencourt@users.noreply.github.com>
2022-05-10 21:40:31 -0300

    reflesh app

--- NLW/web/src/pages/Building/index.tsx
+++ NLW/web/src/pages/Building/index.tsx
@@ -19,7 +19,7 @@ export function Building({namePage}: BuildingProps) {
           Por favor, acesse a área de <NavLink to='/feedbacks' className="text-brand-500 underline underline-offset-2 cursor-pointer">feedbacks.</NavLink>
         </p>
         <p className="text-[#09090A] dark:text-white font-light text-base sm:text-2xl md:text-3xl m-4 px-10">
-          Obrigado!
+          Obrigado!!
         </p>
       </div>
       <Widget />
