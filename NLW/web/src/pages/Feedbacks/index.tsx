87283fb4c03641fd30157a4acdbd69a1c943b7f2
WBitencourt <51727640+WBitencourt@users.noreply.github.com>
2022-05-10 23:05:09 -0300

    get api page feedbacks

--- NLW/web/src/pages/Feedbacks/index.tsx
+++ NLW/web/src/pages/Feedbacks/index.tsx
@@ -1,9 +1,50 @@
+import { api } from '../../lib/api';
+import { useEffect, useState } from 'react';
 import { Building } from '../Building'
 
+interface Feedbacks {
+  id: string;
+  type: string;
+  comment: string;
+  screenshot: string;
+}
+
 export function Feedbacks() {
+  const [feedbacks, setFeedbacks] = useState<Feedbacks>({} as Feedbacks);
+
+  useEffect(() => {
+    async function getFeedBacks() {
+      const response = await api.get('/feedbacks')
+      const data = await response.data.data;
+      setFeedbacks(data);
+
+      console.log(data);
+    }
+
+    getFeedBacks();
+  }, [])
+
   return (
-    <>
-      <Building namePage='feedback' />
-    </>
+    <div>
+      <br />
+      <p className='text-brand-500'>Página em desenvolvimento</p>
+      <hr />
+      <br />
+      {Object.entries(feedbacks).map(([key, value]) => {
+        return (
+          <>
+            <ul key={key} className="text-[#09090A] dark:text-white">
+              <li>ID: {value.id}</li>
+              <li>Tipo: {value.type}</li>
+              <li>Comentário: {value.comment}</li>
+              <li>Screenshot: {value.screenshot}</li>
+            </ul>
+            <hr />
+            <br />
+          </>
+
+        )
+      })}
+    </div>
   );
 }
